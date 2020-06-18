89afe440e60d36304e36036cdc0f6fe7a2eb8887
Andres <jarodriguezh0@gmail.com>
2020-06-17 21:35:53 -0500

    Second Commit

---
+++ app/src/main/java/co/and/ejemploarquitecturamvvmandroid/NoteDao.java
@@ -0,0 +1,37 @@
+package co.and.ejemploarquitecturamvvmandroid;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.FtsOptions;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface NoteDao {
+    //data access object DAO
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+
+
+
+
+
+
+}
--- app/src/main/java/co/and/ejemploarquitecturamvvmandroid/NoteDataBase.java
+++ app/src/main/java/co/and/ejemploarquitecturamvvmandroid/NoteDataBase.java
@@ -1,4 +1,27 @@
 package co.and.ejemploarquitecturamvvmandroid;
 
-public class NoteDataBase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities ={Note.class},version = 1)
+public abstract class NoteDataBase extends RoomDatabase {
+
+    private static NoteDataBase instance;
+
+    public abstract Note noteDao();
+
+    public static synchronized NoteDataBase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+            NoteDataBase.class,"note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+
 }
