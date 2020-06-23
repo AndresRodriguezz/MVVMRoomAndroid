105910df9259b457be188905af661312e4f522b0
Andres <jarodriguezh0@gmail.com>
2020-06-22 19:04:15 -0500

    Added insert Note

---
+++ app/src/main/java/co/and/ejemploarquitecturamvvmandroid/NoteAdapter.java
@@ -0,0 +1,55 @@
+package co.and.ejemploarquitecturamvvmandroid;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = notes.get(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes){
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.txt_view_title);
+            textViewDescription = itemView.findViewById(R.id.txt_view_description);
+            textViewPriority = itemView.findViewById(R.id.txt_view_priority);
+        }
+    }
+}
---
+++ app/src/main/java/co/and/ejemploarquitecturamvvmandroid/NoteRepository.java
@@ -0,0 +1,99 @@
+package co.and.ejemploarquitecturamvvmandroid;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allnotes;
+
+    public NoteRepository(Application application){
+        NoteDataBase dataBase = NoteDataBase.getInstance(application);
+        noteDao = dataBase.noteDao();
+        allnotes = noteDao.getAllNotes();
+    }
+
+    public void insert (Note note){
+        new InsertNoteAsynTask(noteDao).execute(note);
+
+    }
+
+    public void update (Note note){
+        new UpdateNoteAsynTask(noteDao).execute(note);
+
+    }
+
+    public void delete (Note note){
+        new DeleteNoteAsynTask(noteDao).execute(note);
+
+    }
+
+    public void deleteAllNotes(){
+        new DeleteAllNotesAsynTask(noteDao).execute();
+
+    }
+
+    public LiveData<List<Note>> getAllnotes(){
+        return allnotes;
+    }
+
+    private static class InsertNoteAsynTask extends AsyncTask<Note,Void,Void>{
+        private NoteDao noteDao;
+
+        private InsertNoteAsynTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsynTask extends AsyncTask<Note,Void,Void>{
+        private NoteDao noteDao;
+
+        private UpdateNoteAsynTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsynTask extends AsyncTask<Note,Void,Void>{
+        private NoteDao noteDao;
+
+        private DeleteNoteAsynTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsynTask extends AsyncTask<Void,Void,Void>{
+        private NoteDao noteDao;
+
+        private DeleteAllNotesAsynTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+}
